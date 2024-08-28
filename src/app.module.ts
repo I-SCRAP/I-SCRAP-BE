a752a7c8eb34489e6b3f45fb10a5f3992ba0e42e
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2024-08-28 20:05:42 +0900

    Revert "Feature/review archiving (#1)"
    This reverts commit 40e521469017fec8c217b544b3d59b9b912bd223.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`.env`],
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_URI, {
-      dbName: process.env.MONGODB_DBNAME,
-    }),
-    ReviewsModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
