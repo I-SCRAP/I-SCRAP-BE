d4f78249954f014d5029ba0d0f1e6b207d0614ec
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2024-08-28 20:15:44 +0900

    Revert "Feature/review archiving (#1)" (#3)
    This reverts commit 40e521469017fec8c217b544b3d59b9b912bd223.

--- src/utils/validation-utils.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestException } from '@nestjs/common';
-
-export function validateRequiredField(field: string, value: any): void {
-  if (!value) {
-    throw new BadRequestException(`${field} is required.`);
-  }
-}
