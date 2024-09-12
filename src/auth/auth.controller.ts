722a881b2a940aa3f718adbdc5bf75d45f7da138
EunjinWoo <esther0904@naver.com>
2024-09-13 03:30:14 +0900

    Fix: cross-site cookie sending allowance

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -73,6 +73,7 @@ export class AuthController {
       httpOnly: true,
       secure: true, // 로컬 환경에서는 false, 배포 환경에서는 true로 설정
       maxAge: 1000 * 60 * 60 * 24, // 1일 (24시간)
+      sameSite: 'None', // 크로스사이트에서 쿠키 전송을 허용
     });
 
     // 구글에서 받은 Refresh Token을 쿠키에 저장
@@ -80,6 +81,7 @@ export class AuthController {
       httpOnly: true,
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7일 (7일간 유효)
+      sameSite: 'None', // 크로스사이트에서 쿠키 전송을 허용
     });
 
     // 리다이렉트
