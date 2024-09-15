de7176928cb65bcba225da0b5031b4a9d0338ab4
seoyeonjiin <duswls214@khu.ac.kr>
2024-09-16 01:25:06 +0900

    Feat: add getFilteredPopups API

--- src/search/search.repository.ts
+++ src/search/search.repository.ts
@@ -1,11 +1,110 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Bookmark } from 'src/bookmarks/entities/bookmarks.entity';
 import { Popup } from 'src/popups/entities/popup.entity';
+import { Review } from 'src/reviews/entities/review.entity';
+import { SortBy } from './dto/filter.dto';
+import { ObjectId } from 'mongodb';
 
 export class SearchRepository {
   constructor(
     @InjectModel(Popup.name) private readonly popupModel: Model<Popup>,
+    @InjectModel(Bookmark.name) private readonly bookmarkModel: Model<Bookmark>,
+    @InjectModel(Review.name) private readonly reviewModel: Model<Review>,
   ) {}
+
+  async findFilteredPopups(userId: string, query: any): Promise<Popup[]> {
+    return this.popupModel
+      .aggregate([
+        { $match: query },
+        {
+          $lookup: {
+            from: 'bookmarks',
+            localField: '_id',
+            foreignField: 'popupId',
+            as: 'bookmarks',
+          },
+        },
+        {
+          $addFields: {
+            isBookmarked: {
+              $in: [new ObjectId(userId), '$bookmarks.userId'],
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            id: '$_id',
+            address: '$location.address',
+            name: 1,
+            poster: 1,
+            category: 1,
+            isBookmarked: 1,
+            dateRange: {
+              start: {
+                $dateToString: {
+                  format: '%Y/%m/%d',
+                  date: '$dateRange.start',
+                  timezone: '+09:00',
+                },
+              },
+              end: {
+                $dateToString: {
+                  format: '%Y/%m/%d',
+                  date: '$dateRange.end',
+                  timezone: '+09:00',
+                },
+              },
+            },
+          },
+        },
+      ])
+      .exec();
+  }
+
+  async sortPopups(popups: Popup[], sortBy: SortBy): Promise<Popup[]> {
+    if (sortBy === SortBy.POPULARITY) {
+      const popupIds = popups.map((popup) => popup.id);
+      console.log(popupIds);
+      const bookmarks = await this.bookmarkModel
+        .aggregate([
+          { $match: { popupId: { $in: popupIds } } },
+          { $group: { _id: '$popupId', count: { $sum: 1 } } },
+        ])
+        .exec();
+
+      const bookmarkMap = new Map<string, number>(
+        bookmarks.map((b) => [b._id.toString(), b.count]),
+      );
+
+      return popups.sort(
+        (a, b) =>
+          (bookmarkMap.get(b.id.toString()) || 0) -
+          (bookmarkMap.get(a.id.toString()) || 0),
+      );
+    } else if (sortBy === SortBy.RATING) {
+      const popupIds = popups.map((popup) => popup.id);
+      const reviews = await this.reviewModel
+        .aggregate([
+          { $match: { popupId: { $in: popupIds }, isPublic: true } },
+          { $group: { _id: '$popupId', avgRating: { $avg: '$rating' } } },
+        ])
+        .exec();
+
+      const reviewMap = new Map<string, number>(
+        reviews.map((r) => [r._id.toString(), r.avgRating]),
+      );
+
+      return popups.sort(
+        (a, b) =>
+          (reviewMap.get(b.id.toString()) || 0) -
+          (reviewMap.get(a.id.toString()) || 0),
+      );
+    }
+
+    return popups;
+  }
 
   async searchPopupsByName(popupName: string) {
     const popups = await this.popupModel.aggregate([
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SearchRepository } from './search.repository';
+import { FilterDto, Location, SortBy } from './dto/filter.dto';
 
 @Injectable()
 export class SearchService {
@@ -8,5 +9,54 @@ export class SearchService {
   async searchPopupsByName(popupName: string) {
     const popups = await this.searchRepository.searchPopupsByName(popupName);
     return popups;
+  }
+
+  async getFilteredPopups(userId: string, filterDto: FilterDto) {
+    const {
+      sortBy = SortBy.POPULARITY,
+      start,
+      end,
+      location,
+      category,
+    } = filterDto;
+
+    const query: any = {};
+
+    const now = new Date();
+    if (start || end) {
+      const startDate = start ? new Date(start) : null;
+      const endDate = end ? new Date(end) : null;
+
+      if (startDate) query['dateRange.start'] = { $lte: endDate || now };
+      if (endDate) query['dateRange.end'] = { $gte: startDate || now };
+    }
+
+    if (location) {
+      if (location.includes(Location.ALL_REGIONS)) {
+        delete query['location.address'];
+      } else if (location.includes(Location.OTHER_REGIONS)) {
+        query['location.address'] = {
+          $not: {
+            $regex: `^(${Location.JAMSIL}|${Location.SEONGSU}|${Location.HONGDAE}|${Location.JONGNO}|${Location.YONGSAN}|${Location.THEHYUNDAI}|${Location.GYEONGGI_REGION})`,
+            $options: 'i',
+          },
+        };
+      } else {
+        query['location.address'] = {
+          $in: location.map((loc) => new RegExp(loc, 'i')),
+        };
+      }
+    }
+
+    if (category) {
+      query.category = { $in: category };
+    }
+
+    const popups = await this.searchRepository.findFilteredPopups(
+      userId,
+      query,
+    );
+    const sortedPopups = await this.searchRepository.sortPopups(popups, sortBy);
+    return sortedPopups;
   }
 }
