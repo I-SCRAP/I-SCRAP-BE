0f9847f5db9f9eaffd38d508894ca6ec51a81351
EunjinWoo <esther0904@naver.com>
2024-09-24 13:46:57 +0900

    Feat: login properly connected w/ FE

--- src/lambda.ts
+++ src/lambda.ts
@@ -10,7 +10,10 @@ export const handler = async (event, context) => {
   if (!cachedServer) {
     console.log(event);
     const nestApp = await NestFactory.create(AppModule);
-    nestApp.enableCors();
+    nestApp.enableCors({
+      origin: ['https://i-scrap-fe.vercel.app', 'http://localhost:3000'], // 허용할 프론트엔드 도메인
+      credentials: true, // 인증 정보를 포함한 요청 허용 (쿠키 포함)
+    });
     nestApp.use(cookieParser());
     nestApp.useGlobalPipes(
       new ValidationPipe({
