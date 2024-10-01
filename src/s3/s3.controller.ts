759e88193e241070d5b7ddc7875bc8f660fcf281
seoyeonjiin <duswls214@khu.ac.kr>
2024-10-02 03:03:03 +0900

    Fix: remove temporary userId

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Param, UseGuards } from '@nestjs/common';
+import { Controller, Post, Param, UseGuards, Req } from '@nestjs/common';
 import { S3Service } from './s3.service';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
@@ -9,9 +9,10 @@ export class S3Controller {
 
   @Post('upload-url/:fileName')
   async getPresignedUploadUrl(
+    @Req() req,
     @Param('fileName') fileName: string,
   ): Promise<{ uploadUrl: string }> {
-    const userId = '66b4b5d2f9415815acd65e6a';
+    const userId = req.user.id;
     const bucketName = process.env.S3_USER_BUCKET;
     const uploadUrl = await this.s3Service.generatePresignedUploadUrl(
       bucketName,
