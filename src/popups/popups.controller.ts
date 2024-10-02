f0d8fa4c717b6771fdb6d27a0fdccb1e0e7b4337
EunjinWoo <esther0904@naver.com>
2024-10-02 16:24:27 +0900

    Fix: modified user & popups module to throw 401 error when id_token is invalid

--- src/popups/popups.controller.ts
+++ src/popups/popups.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { PopupsService } from './popups.service';
 import { validateRequiredField } from 'src/utils/validation-utils';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
@@ -16,6 +24,13 @@ export class PopupsController {
   @UseGuards(JwtAuthGuard)
   @Get('home/personalized-popups')
   async getPersonalizedPopups(@Req() req) {
+    // req.user가 존재하지 않으면 UnauthorizedException 던지기
+    if (!req.user) {
+      throw new UnauthorizedException(
+        '유효하지 않은 ID Token이거나 사용자 정보를 찾을 수 없습니다.',
+      );
+    }
+
     const userName = req.user?.name;
     const popups = await this.popupsService.getPersonalizedPopups();
     return {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { UsersService } from './users.service';
 import { BookmarksService } from 'src/bookmarks/bookmarks.service';
@@ -14,6 +20,13 @@ export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   async getProfile(@Req() req: Request) {
+    // req.user가 존재하지 않으면 UnauthorizedException 던지기
+    if (!req.user) {
+      throw new UnauthorizedException(
+        '유효하지 않은 ID Token이거나 사용자 정보를 찾을 수 없습니다.',
+      );
+    }
+
     const user = req.user as any; // 타입 캐스팅
 
     // 유저의 리뷰 통계 정보 (리뷰 개수 및 받은 좋아요 수)
