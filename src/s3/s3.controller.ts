a7bfbd5926b2931b62583e722ee153c3fe3d6319
서연진(yonjaan) <68274803+seoyeonjin@users.noreply.github.com>
2024-10-02 16:16:07 +0900

    Feature/s3 (#36)
    * Feat: set userId dynamically from req object
    
    * Fix: remove temporary userId

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Post, Param } from '@nestjs/common';
+import { Controller, Post, Param, UseGuards, Req } from '@nestjs/common';
 import { S3Service } from './s3.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('files')
 export class S3Controller {
   constructor(private readonly s3Service: S3Service) {}
 
   @Post('upload-url/:fileName')
   async getPresignedUploadUrl(
+    @Req() req,
     @Param('fileName') fileName: string,
   ): Promise<{ uploadUrl: string }> {
-    const userId = '66b4b5d2f9415815acd65e6a';
+    const userId = req.user.id;
     const bucketName = process.env.S3_USER_BUCKET;
     const uploadUrl = await this.s3Service.generatePresignedUploadUrl(
       bucketName,
