b67156bb4a671b70e9998d5a3e391569fd177bad
esther <92513850+EunjinWoo@users.noreply.github.com>
2024-10-03 23:28:17 +0900

    Fix: apply email-template engine(handlebars) & set scheduling time to 9AM (#50)
    * Fix: logout available
    
    * Fix: apply email-template engine(handlebars) & set scheduling time to 9AM

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import * as handlebars from 'handlebars';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class MailService {
@@ -15,14 +18,31 @@ export class MailService {
     });
   }
 
-  async sendEmail(to: string, subject: string, html: string) {
+  // 템플릿 파일 읽기
+  private readHTMLTemplate(fileName: string): string {
+    const filePath = path.join(__dirname, '..', 'templates', fileName);
+    return fs.readFileSync(filePath, 'utf8');
+  }
+
+  // 이메일 전송 메서드
+  async sendEmail(
+    to: string,
+    subject: string,
+    templateName: string,
+    templateData: object,
+  ) {
     try {
+      const htmlTemplate = this.readHTMLTemplate(templateName); // 템플릿 파일 로드
+      const template = handlebars.compile(htmlTemplate); // Handlebars 템플릿 컴파일
+      const htmlToSend = template(templateData); // 데이터와 템플릿 결합
+
       const mailOptions = {
         from: process.env.GMAIL_USER,
         to,
         subject,
-        html,
+        html: htmlToSend, // 템플릿이 적용된 HTML
       };
+
       await this.transporter.sendMail(mailOptions);
       console.log(`Email sent to ${to}`);
     } catch (error) {
